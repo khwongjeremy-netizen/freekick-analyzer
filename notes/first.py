3635a90d1c2b75876f616339c3dc0ffb83210dc9
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-01-09 01:34:01 +0000

    adfdsaf

---
+++ notes/first.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import cv2
+from glob import glob
+import IPython.display as ipd
+from tqdm.notebook import tqdm
+
+import subprocess
+input_file = ''
+sub.process.run(['ffmpeg', '-1', input_file, -qscale, '0', '', '-loglevel', 'quiet'])
+
+ipd.Video(' ',)
