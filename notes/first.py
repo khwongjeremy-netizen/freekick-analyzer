9088618b0af7ad82e0d209750c9b0f7ddf1fb8e5
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-01-09 02:05:16 +0000

    sdfdsfds

--- notes/first.py
+++ notes/first.py
@@ -10,3 +10,19 @@ input_file = ''
 sub.process.run(['ffmpeg', '-1', input_file, -qscale, '0', '', '-loglevel', 'quiet'])
 
 ipd.Video(' ',)
+cap = cv2.videoCapture('')
+#Total number of frames in video 
+cap.get(cv2.cv2_PROP_FRAME_COUNT )
+
+#Gets the height and width of the video
+height = cap.get(cv2.cv2.CAP_PROP_FRAME_HEIGHT)
+width = cap.getg(cv2.cv2.CAP_PROP_FRAME_WIDTH)
+print(f'Height {height}, Width {width}')
+
+#Get frame per second 
+fps = cap.get(cv2.cv2.CAP_PROP_FPS)
+print(f'FPS : {fps:0.2f}')
+
+ret, img = cap.read()
+
+cap.release()
---
+++ notes/tutorials.md
@@ -0,0 +1,3 @@
+https://youtu.be/AxIc-vGaHQ0?si=zGDzv3WrgJBUct_q 
+
+https://www.kaggle.com/code/robikscube/working-with-video-in-python-youtube-tutorial/notebook 
