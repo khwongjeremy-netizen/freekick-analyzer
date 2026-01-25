c2a3736b54850ca0323c1b5198eb8abf3035517c
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-01-25 02:53:03 +0000

    asfgfd

--- freekick.py
+++ freekick.py
@@ -1,38 +1,13 @@
-import pandas as pd
-import numpy as np
 import cv2
-from glob import glob
-import IPython.display as ipd
-from tqdm.notebook import tqdm
-import subprocess
-import tkinter as tk
-import func
+import numpy as np
 
+cap = cv2.VideoCapture(0)
 
-kick_shot = cv2.VideoCapture('freekick.mp4')
-ret, img = kick_shot.read()
-func.display_cv2_img(img)
-kick_shot.release()
-#Shot of the freekick's foot moment of impact
+while True:
+    ret, frame = cap.read()
 
-'''
-labels = pd.read_csv('',
-                     low_memory=False)
-video_labels = (
-    labels.query('videoName == ""').reset_index(drop=True).copy()
-)
-video_labels["video_frame"] = (video_labels["frameIndex"] * 11.9).round().astype("int")
+    cv2.imshow('frame', frame)
 
-
-
-
-Next task:
-    -record clips of freekicks next time playing football
-    -Test if I can annotate and pull images from clip
-
-
-
-Goal: 
-    -display the video on a window with tkinter
-    -Calculate angles of freekicks(foot, ball, runup)
-'''
+    if cv2.waitkey(1) == ord('q'):
+        break
+cap.release()
